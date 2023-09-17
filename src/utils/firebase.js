e883cbe3cfae866eaf5a8a9019701bd73025c0a2
naveensai <sainaveen9000@gmail.com>
2023-09-17 21:06:03 +0530

    firebase

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyAfVFkulxOr_STBZiCqVTGvpAt47yW-Dbg',
+  authDomain: 'netflixgpt-19752.firebaseapp.com',
+  projectId: 'netflixgpt-19752',
+  storageBucket: 'netflixgpt-19752.appspot.com',
+  messagingSenderId: '721490948255',
+  appId: '1:721490948255:web:1f122a090102ff4d3632aa',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
